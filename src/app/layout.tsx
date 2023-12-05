c6422274c6cbc4507f328c277c31086e068bd30a
robsonnsbr <robsonnsbr@gmail.com>
2023-12-05 19:37:57 -0300

    initial: landing-page

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from 'next';
+import { Fira_Code, Inter } from 'next/font/google';
+import './globals.css';
+const firaCode = Fira_Code({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] });
+
+interface children {
+  children: React.ReactNode;
+}
+
+const isValidFont = () => {
+  if (firaCode) {
+    return firaCode;
+  }
+
+  if (inter) {
+    return inter;
+  }
+  console.log(new Error('Font is not valid'));
+};
+
+export const metadata: Metadata = {
+  title: 'Landing Page',
+  description: 'Landing Page for leaning nextJs-14'
+};
+
+export default function RootLayout({ children }: children) {
+  return (
+    <html lang="pt-BR">
+      <link rel="icon" href="./favicon.ico" sizes="any" />
+      <body
+        suppressHydrationWarning={true}
+        className={isValidFont() ? isValidFont().className : 'font-sans'}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>TESTE MAIN CSS {'=> ==='} const</h1>
+      <div>
+        <button className="text-black bg-white">Botão</button>
+        <ul>
+          <li>Hello</li>
+          <li>Hello</li>
+          <li>Hello</li>
+          <a href="/">teste link</a>
+        </ul>
+      </div>
+    </main>
+  );
+}
