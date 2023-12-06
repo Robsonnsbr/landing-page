20e48787a465b048ef8ea486a8682fa60c04d546
robsonnsbr <robsonnsbr@gmail.com>
2023-12-06 00:21:16 -0300

    initial: including the first element in the list of

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,16 @@
-import Header from '../components/Header';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <hr />
       <main>
-        <h1>TESTE MAIN CSS {'=> ==='} const</h1>
-        <>
-          <button className="text-black bg-white">Botão</button>
-          <ul>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <a href="/">teste link</a>
-          </ul>
-        </>
+        <div>MAIN</div>
       </main>
+      <hr />
+      <Footer />
     </>
   );
 }
