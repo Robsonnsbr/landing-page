2240086473506a7cde8b1390a7bebd3777133558
robsonnsbr <robsonnsbr@gmail.com>
2023-12-05 23:30:23 -0300

    initial: cfg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
+import Header from '../components/Header';
+
 export default function Home() {
   return (
-    <main>
-      <h1>TESTE MAIN CSS {'=> ==='} const</h1>
-      <div>
-        <button className="text-black bg-white">Botão</button>
-        <ul>
-          <li>Hello</li>
-          <li>Hello</li>
-          <li>Hello</li>
-          <a href="/">teste link</a>
-        </ul>
-      </div>
-    </main>
+    <>
+      <Header />
+      <main>
+        <h1>TESTE MAIN CSS {'=> ==='} const</h1>
+        <>
+          <button className="text-black bg-white">Botão</button>
+          <ul>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <a href="/">teste link</a>
+          </ul>
+        </>
+      </main>
+    </>
   );
 }
